752fe05e293a8f3a37889fcb7d3d3e0a93892e7c
isaquelopes047 <isaque.lopes047@gmail.com>
2021-08-02 21:48:06 -0300

    atualização

--- JS/logado.js
+++ JS/logado.js
@@ -1,4 +1,7 @@
 
+let nameLogado = document.querySelector('.nameUser');
+nameLogado.textContent = `${localStorage.nome}`
+
 let bemVindo = document.querySelector('.bemVindo');
 
-bemVindo.textContent = `Bem vindo ${localStorage.nome}`;
+bemVindo.textContent = `Bem vindo ${localStorage.nome}.`;
